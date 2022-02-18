ed8be93b7493497eee9157030b021262deb37d25
Mehedi Hasan Chonchol <mehedi.chonchol@gmail.com>
2022-02-18 09:20:30 +0600

    PowerShell Script for Export and Import in AWS RDS
    Two Scripts are there for Exporting and Importing data from AWS RDS MySQL. See readme for details.

---
+++ SP_5min_10min.ps1
@@ -0,0 +1,99 @@
+##DB to DB data Migrator (Stored Procedure)
+##Developer: Mehedi Hasan Chonchol
+##Date: 08-02-2022
+##Contact: mehedi.chonchol@gmail.com
+
+
+#Locating Connector
+[void][System.Reflection.Assembly]::LoadFrom(“C:\Program Files (x86)\MySQL\MySQL Connector Net 8.0.28\Assemblies\v4.8\MySql.Data.dll”)
+
+#DB Connection Declaration
+$sourceDBConnection = New-Object MySql.Data.MySqlClient.MySqlConnection
+
+#DB Connection String
+$sourceDBConnection.ConnectionString = “server=localhost;user id=root;password=;database=upwork_29457854_01;pooling=false”
+
+#Source Procedure Start
+$sourceDBConnection.Open()
+Write-Output "Database Connected..."
+
+$exportProcedureCommand = New-Object MySql.Data.MySqlClient.MySqlCommand
+$exportProcedureCommand.Connection = $sourceDBConnection
+
+$expProcScript = "    
+    DROP PROCEDURE IF EXISTS `Export_Procedure`;
+    
+    CREATE PROCEDURE `Export_Procedure` ()
+        
+    BEGIN
+	    
+        DROP TABLE IF EXISTS 5min_10min;
+
+        -- Create 5min_10min TABLE in Source DB
+	    CREATE TABLE `5min_10min` (
+	      id int(11) NOT NULL AUTO_INCREMENT,
+	      agent_id varchar(255) DEFAULT NULL,
+	      over5 bigint(20) DEFAULT NULL,
+	      over10 bigint(20) DEFAULT NULL,
+	      logontime bigint(20) DEFAULT NULL,
+	      Date datetime DEFAULT NULL,
+	      PRIMARY KEY (id)
+	    )
+	    ENGINE = INNODB,
+	    CHARACTER SET latin1,
+	    COLLATE latin1_swedish_ci;
+
+	    -- INSERT data from TWO TABLES into 5min_10min TABLE
+	    INSERT INTO 5min_10min (agent_id, over5, over10, logontime, date)
+	    SELECT A.agent_id
+	    , CASE WHEN A.over5 IS NULL
+		    THEN 0
+		    ELSE A.over5
+	    END AS over5
+	    , CASE WHEN A.over10 IS NULL
+		    THEN 0
+		    ELSE A.over10
+	    END AS over10
+	    , B.logontime
+	    , DATE_FORMAT(B.date, '%Y-%m-%d') AS date
+	    FROM (
+		    select lrs.agent_id, 
+		    IFNULL(SUM(CASE WHEN lrs.talk_time > 300000 THEN 1 ELSE 0 END), 0) AS over5 
+		    ,IFNULL(SUM(CASE WHEN lrs.talk_time > 600000 THEN 1 ELSE 0 END), 0) AS over10
+		    ,FROM_UNIXTIME(start_date/1000, '%Y-%m-%d') AS date
+		    FROM lnm_r_session lrs where lrs.agent_id is not null and lrs.start_date >= (unix_timestamp(current_date) - 0 * 60 * 60 * 24) * 1000 AND lrs.start_date < (unix_timestamp(current_date) - (0 - 1) * 60 * 60 * 24) * 1000 
+		    AND lrs.agent_group IN ('ni', 'closer')
+		    group by lrs.agent_id
+	    ) A
+	    RIGHT JOIN (
+		    select las.agent_id, 
+		    SUM(las.state_duration_time/3600000) AS logontime
+		    ,FROM_UNIXTIME(state_changed_date/1000, '%Y-%m-%d') AS date
+		    FROM  lnm_m_agent_status las
+		    WHERE las.state_changed_date >= (unix_timestamp(current_date) - 0 * 60 * 60 * 24) * 1000 AND las.state_changed_date < (unix_timestamp(current_date) - (0 - 1) * 60 * 60 * 24) * 1000
+		    AND las.agent_group IN ('ni', 'closer')
+		    AND las.state IN (1, 3, 4, 5)
+		    group by las.agent_id, date
+	    ) B ON A.agent_id = B.agent_id AND A.date = B.date;
+	    
+	    COMMIT;
+
+    END;"
+
+$exportProcedureCommand.CommandText = $expProcScript
+
+
+#Source Query Execution
+Write-Output "Source Data Preparation Started..."
+$myreader = $exportProcedureCommand.ExecuteReader()
+
+if($myreader.Read()){
+    Write-Output "Source Data Preparation Error..."
+    $myreader.GetString(0)
+} else {
+    $myreader.Close()
+    Write-Output "Export Procedure Stored Successfully..:)"
+}
+
+$sourceDBConnection.Close()
+Write-Output "DB Connection Closed..!"
---
+++ Scheduler_5min_10min.ps1
@@ -0,0 +1,153 @@
+﻿##DB to DB data Migrator (Daily Scheduler)
+##Developer: Mehedi Hasan Chonchol
+##Date: 08-02-2022
+##Contact: mehedi.chonchol@gmail.com
+
+
+#Locating Connector
+[void][System.Reflection.Assembly]::LoadFrom(“C:\Program Files (x86)\MySQL\MySQL Connector Net 8.0.28\Assemblies\v4.8\MySql.Data.dll”)
+
+#DB Connection String Declaration
+$sourceDBConnString="server=localhost; uid=root; pwd=; database=upwork_29457854_01; Pooling=False; convert zero datetime=True"
+$destDBConnString="server=localhost; uid=root; pwd=; database=test; Pooling=False; convert zero datetime=True"
+
+##SOURCE DATA PREPARATION TASK
+$sourceDB = New-Object MySql.Data.MySqlClient.MySqlConnection($sourceDBConnString)
+
+$sourceDB.Open()
+Write-Output "Source DB Connected..."
+
+$expProcCmd = New-Object MySql.Data.MySqlClient.MySqlCommand
+$expProcCmd.Connection = $sourceDB
+$expProcCmd.CommandText = "CALL Export_Procedure();"
+
+$expReader = $expProcCmd.ExecuteReader()
+
+if($expReader.Read()){
+    Write-Output "Source Procedure Execution Failed..."
+    $expReader.GetString(0)
+} else {    
+    $expReader.Close()
+    $sourceDB.Close()
+    Write-Output "Source Procedure Execution Successful...! :)"
+}
+
+Write-Output "Preparing Source Data..."
+$sourceDB.Open()
+$selectQuery = "SELECT * FROM 5min_10min;"
+
+$req = New-Object Mysql.Data.MysqlClient.MySqlCommand($selectQuery, $sourceDB)
+
+$dataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($req)
+
+$dataSet = New-Object System.Data.DataSet
+
+#Data is stored here in Powershell DataSet
+$dataAdapter.Fill($dataSet, "5min10min") | Out-Null
+Write-Output "Data Preparation Successful..."
+
+#$dataSet.Tables["5min10min"] | Export-Csv -path "E:/2022/upwork/29457854/5min_10min.csv" -NoTypeInformation
+$sourceDB.Close()
+Write-Output "Source DB Connection Closed...!"
+
+##DESTINATION DATA INSERTION TASK
+$destDB = New-Object MySql.Data.MySqlClient.MySqlConnection($destDBConnString)
+$destDB.Open()
+Write-Output "Destination DB Connected..."
+
+#DROP & CREATE Temp Table
+$emptyTempTable = New-Object MySql.Data.MySqlClient.MySqlCommand
+$emptyTempTable.Connection = $destDB
+$emptyTempTable.CommandText = 
+    "DROP TABLE IF EXISTS 5min_10min_temp;
+
+    CREATE TABLE `5min_10min_temp` (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        agent_id varchar(255) DEFAULT NULL,
+        over5 bigint(20) DEFAULT NULL,
+        over10 bigint(20) DEFAULT NULL,
+        logontime bigint(20) DEFAULT NULL,
+        Date datetime DEFAULT NULL,
+        PRIMARY KEY (id)
+    );"
+
+
+$tempReader = $emptyTempTable.ExecuteReader()
+
+if($tempReader.Read()){
+    Write-Output "Temp Table Creation Error...!"
+    $tempReader.GetString(0)
+} else {    
+    $tempReader.Close()
+}
+
+$destDB.Close()
+
+$destDB.Open()
+#Insert Data into Temp Table
+$impCmd = New-Object MySql.Data.MySqlClient.MySqlCommand
+$impCmd.Connection = $destDB
+
+#Looping into the results
+Write-Output "Data Insertion Started..."
+foreach($row in $dataset.Tables.Rows){
+
+    $agent_id = $row.agent_id
+    $over5 = $row.over5
+    $over10 = $row.over10
+    $logontime = $row.logontime
+    $date = [DateTime]$row.Date
+    
+    $fdate = '{0:yyyy-MM-dd}' -f $date
+    $agent_id = $agent_id.Replace("'", "\'")
+
+    Write-Output $row
+    Write-Output $row.Date.DateTime
+        
+    $insertQuery="INSERT INTO 5min_10min_temp (agent_id, over5, over10, logontime, Date)
+    VALUES ('$agent_id', '$over5', '$over10', '$logontime', DATE_FORMAT('$fdate', '%Y-%m-%d'));"
+
+    Write-Output $insertQuery
+
+    $impCmd.CommandText = $insertQuery
+
+    $myreader = $impCmd.ExecuteReader()
+
+    if($myreader.Read()){
+        Write-Output "DB Record Insertion Error...!"
+        $myreader.GetString(0)
+    } else {
+        $myreader.Close()
+    }
+}
+
+$destDB.Close()
+
+$destDB.Open()
+Write-Output "Data Inserted into TEMP Table..."
+
+#INSERT into MAIN Table
+Write-Output "Main Table Data INSERTION Started..."
+$finalInsert = New-Object MySql.Data.MySqlClient.MySqlCommand
+$finalInsert.Connection = $destDB
+$finalInsert.CommandText = 
+    "INSERT INTO 5min_10min(agent_id, over5, over10, logontime, Date)
+    SELECT agent_id, over5, over10, logontime, DATE_FORMAT(date, '%Y-%m-%d')
+    FROM 5min_10min_temp
+    WHERE CONCAT(agent_id, Date) NOT IN (
+	    SELECT CONCAT(agent_id, Date) FROM 5min_10min
+    );"
+
+
+$finalReader = $finalInsert.ExecuteReader()
+
+if($finalReader.Read()){
+    Write-Output "Main Table Data Insertion Error...!"
+    $finalReader.GetString(0)
+} else {    
+    $finalReader.Close()
+    Write-Output "DB Record Insertion Successful...:)"
+}
+
+$destDB.Close()
+Write-Output "Destination DB Connection Closed...!"
